005cbc67f80ee85403ba225ca5593e719b8e47fa
Tisinti <schmidt.erik02@protonmail.com>
2023-11-27 20:39:58 +0100

    trying gridsearch with svr

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ rf = RandomForestRegressor(criterion='friedman_mse', max_features='sqrt')
 lr = LinearRegression()
 svr = SVR()
 
+svr_param = {
+    "kernel": ['poly', 'rbf', 'sigmoid'],
+    "degree": [1, 2, 3],
+    "gamma": ['scale'],
+    "coef0": [-0.5, 0, 0.5],
+    "C": [0.3, 0.5, 1]
+    }
+rf_param = {
+    "min_samples_leaf": [0.75, 1],
+    "max_features":["log2","sqrt"],
+    "criterion": ['friedman_mse', 'squared_error', 'absolute_error', 'poisson'],
+    }
+
+clf = GridSearchCV(svr, param_grid=svr_param, cv = 5, n_jobs= -1,
+                   scoring="neg_mean_absolute_error", verbose=2)
+
 def train(model):
     run_training(after_covid, model)
 
@@ -25,7 +41,7 @@ def eval(model):
     return 
 
 def predict(model):
-    search = "I Origin"
+    search = "I Origins"
     date = "28.11.2023"
     sel_model = get_model(model)
     enc = get_encoder()
@@ -33,6 +49,5 @@ def predict(model):
     print(predict_attendance(search, date, sel_model, enc))
 
 if __name__ == "__main__":
-   predict(lr)
+   #train(clf)
    predict(rf)
-   predict(svr)
