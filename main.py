405db4916003e94793958c9fd6be7afd0751d424
Tisinti <schmidt.erik02@protonmail.com>
2023-11-27 19:54:38 +0100

    added standard SVR

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from sklearn.linear_model import Lasso, LinearRegression
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
 
 after_covid = get_after_covid()
 
 rf = RandomForestRegressor(criterion='friedman_mse', max_features='sqrt')
 lr = LinearRegression()
+svr = SVR()
 
 def train(model):
     run_training(after_covid, model)
@@ -23,15 +25,14 @@ def eval(model):
     return 
 
 def predict(model):
-    search = "Mona Lisa and the Blood Moon"
-    date = "05.12.2023"
+    search = "I Origin"
+    date = "28.11.2023"
     sel_model = get_model(model)
     enc = get_encoder()
     
     print(predict_attendance(search, date, sel_model, enc))
 
 if __name__ == "__main__":
-   eval(lr)
-   eval(rf)
    predict(lr)
    predict(rf)
+   predict(svr)
