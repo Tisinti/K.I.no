8fc04d3e7dc9205000a5172b31bf16342689b90b
Tisinti <tisinti@localhost.localdomain>
2024-05-10 21:34:03 +0200

    feat(models): update models

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 from app.preprocessor import export_whole_meta
 from app.preprocessor import execute_export
 from app.analysis import createAllPlotsPipe
-
+from app.training import iterate_models
 
 if __name__ == "__main__":
     #app.run(debug=True)
     #print(iterate_models())
     #execute_export()
     #export_whole_meta()
-    createAllPlotsPipe()
+    #createAllPlotsPipe()
+    iterate_models()
